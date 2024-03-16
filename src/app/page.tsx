21e2ff85ac907437cfc919bc076f6f79d4fac2fb
r00t <r00t@ragonsys.com>
2024-03-16 17:02:16 +0530

    (chore):added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,14 +22,16 @@ export default function Home() {
         className="w-full h-full absolute top-0 left-0 z-0"
       />
       <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md ">
-        <h1 className="md:text-4xl text-2xl lg:text-7xl font-bold text-center text-slate-400 relative z-20">
+        <h1 className="flex flex-row md:text-4xl text-2xl lg:text-7xl font-bold text-center text-slate-400 relative z-20">
           Hi there, I&apos;m <span className="text-slate-200">Naveen Ben</span>!
-          👋
+          <div
+          className="animate-bounce  bottom-0 right-0 z-0 text-6xl text-sky-500"
+          >👋</div>
         </h1>
-        <div className="w-[40rem] h-40 relative ">
-          <div className="absolute inset-x-20 lg:inset-x-40 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-4/5 blur-sm" />
+        <div className="w-[40rem] h-40 relative animate-pulse ">
+          <div className="absolute inset-x-20 lg:inset-x-40 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[4px] w-4/5 blur-sm" />
           <div className="absolute inset-x-20 lg:inset-x-40 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-4/5" />
-          <div className="absolute inset-x-60 lg:lg:inset-x-80 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 lg:lg:inset-x-80 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[10px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 lg:lg:inset-x-80 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-2/4" />
         </div>
         <div className="contact flex lg:flex-row flex-col gap-3 absolute lg:items-center sm:items-start md:items-start  text-xl ">
